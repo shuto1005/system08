19ae2810966daf3126ce41266a119d2a7d77bdac
Chon-Sufan <46482378+Chon-Sufan@users.noreply.github.com>
2020-06-30 15:53:11 +0900

    nothing

--- system08/PriorityProcessingUnit.cs
+++ system08/PriorityProcessingUnit.cs
@@ -6,10 +6,23 @@ namespace system08
 {
     class PriorityProcessingUnit
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int MoveWindow(IntPtr hwnd, int x, int y,int nWidth, int nHeight, int bRepaint);
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
 
         public void assignPriority(int window_id, int priority)
         {
+            System.Diagnostics.Process p =
+            System.Diagnostics.Process.Start("notepad.exe");
+            p.WaitForInputIdle();
+            MoveWindow(p.MainWindowHandle, 0, 10, 300, 200, 1);
+        }
 
+        public int Update(int win_num, int priotiry)
+        {
+
+            return -1;
         }
     }
 }
