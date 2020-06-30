42bf1fa10e9a770dbcba373dc6a9ad487bf06369
Chon-Sufan <46482378+Chon-Sufan@users.noreply.github.com>
2020-06-30 14:30:36 +0900

    add PriorityProcessingUnit

---
+++ system08/PriorityProcessingUnit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace system08
+{
+    class PriorityProcessingUnit
+    {
+
+        public void assignPriority(int window_id, int priority)
+        {
+
+        }
+    }
+}
