a2a59ac389f76c4bfcdc8431b05a063f1117dafc
al18058 <al18058@shibaura-it.ac.jp>
2020-07-04 10:46:16 +0900

    pull

---
+++ system08/NWindow.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Collections.ObjectModel; // ObservableCollection
+using System.Diagnostics;
+
+namespace EditableListViewTest
+{
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<N_Window> NewWindow;
+
+        //-----------------------------------------------------------------------------------------------
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewWindow = new ObservableCollection<N_Window>();
+
+            var nwindow = new NewWindow("", "", "");
+            NewWindow.Add(nwindow);
+
+            
+
+            listView.ItemsSource = NewWindow;
+        }
+    } // end of MainWindow class
+
+    //***********************************************************************************************
+    public class NewWindow
+    {
+        public string ProductName { get; set; }
+        public int Priority { get; set; }
+        public int Volume { get; set; }
+
+        public N_Window(string productName, int priority, int volume)
+        {
+            this.ProductName = productName;
+            this.Priority = priority;
+            this.Volume = volume;
+        }
+
+        public override string ToString()
+        {
+            return this.ProductName + "-" + this.Priority + "-" + this.Volume;
+        }
+    } // end of N_Window class
+} // end of namespace
