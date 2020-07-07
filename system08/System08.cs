8fd764c7f9ee560ba2a412b0808b089cb0fd6a49
al18088 <al18088@shibaura-it.ac.jp>
2020-07-07 16:36:52 +0900

    コンストラクタの追加

---
+++ system08/System08.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace system08
+{
+	public class UIModule
+
+	{
+		List<wdata> data;
+
+		private PriorityModule priorityModule;
+
+
+		public UIModule()
+		{
+			priorityModule = new PriorityModule();
+		}
+		public bool Run()
+		{
+			data = Load();
+			return true;
+		}
+
+		public void Destruct(int order_end)
+		{
+			Save(data);
+		}
+	}
+}
