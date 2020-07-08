a09ab1754ba7f5b0e15970a4169a4e6675861564
al18004 <al18004@sic.shibaura-it.ac.jp>
2020-07-08 18:35:00 +0900

    新規作成

---
+++ system08/GetWindowsWithHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace system08
+{
+    public partial class UIModule
+    {
+        /// <summary>
+        /// GetWindowsから情報を取得し、保存データと照合
+        /// </summary>
+        /// <returns></returns>
+        public List<wdata> GetWindowsWithHistory()
+        {
+            return priorityModule.GetWindows();
+        }
+    }
+}
