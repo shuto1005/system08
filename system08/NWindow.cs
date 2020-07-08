fbd56dcf0d31959b086dfe8461a546af8a93d913
al18058 <al18058@shibaura-it.ac.jp>
2020-07-08 17:10:47 +0900

    スライダーの追加(仮)

--- system08/NWindow.cs
+++ system08/NWindow.cs
@@ -7,16 +7,16 @@ namespace EditableListViewTest
 {
     public partial class MainWindow : Window
     {
-        private ObservableCollection<N_Window> NewWindow;
+        private ObservableCollection<NWindow> NewWindow;
 
         //-----------------------------------------------------------------------------------------------
         public MainWindow()
         {
             InitializeComponent();
 
-            NewWindow = new ObservableCollection<N_Window>();
+            NewWindow = new ObservableCollection<NWindow>();
 
-            var nwindow = new N_Window("", 0, 0);
+            var nwindow = new NewWindow("", "", "");
             NewWindow.Add(nwindow);
 
             
@@ -32,7 +32,7 @@ namespace EditableListViewTest
         public int Priority { get; set; }
         public int Volume { get; set; }
 
-        public N_Window(string productName, int priority, int volume)
+        public NWindow(string productName, int priority, int volume)
         {
             this.ProductName = productName;
             this.Priority = priority;
