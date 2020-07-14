02919d5c4f27186f543669d6b24f0af6db743726
al18004 <al18004@sic.shibaura-it.ac.jp>
2020-07-14 16:23:57 +0900

    新規作成

---
+++ system08/Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace system08
+{
+    public partial class UIModule
+    {
+        /// <summary>
+        /// 引数で与えられたデータを履歴として保持する
+        /// </summary>
+        /// <param name="data"></param>
+        public void StoreAsHistory(wdata data)
+        {
+
+        }
+    }
+}
