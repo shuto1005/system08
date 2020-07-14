ce0ed3e82b0bbf2d2454951dba6e1c2929fac7d0
al18058 <al18058@shibaura-it.ac.jp>
2020-07-14 15:09:55 +0900

    xamlに追加

--- system08/MainWindow.xaml.cs
+++ system08/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
 
 namespace system08
 
@@ -34,12 +35,24 @@ namespace system08
             NewWindow.Add(nwindow);
 
 
-
             listView.ItemsSource = module.managedData;
+
+
+            bool first = true;
+            Activated += (s, e) =>
+            {
+                if (first)
+                {
+                    first = false;
+                    var helper = new System.Windows.Interop.WindowInteropHelper(this);
+                    module.SetThisWindowHandle(helper.Handle);
+                    Init_UI();
+                }
+            };
         }
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            module.OnVolumeChanged(sender,e);
+            module.OnVolumeChanged(sender, e);
         }
 
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
@@ -68,6 +81,139 @@ namespace system08
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             module.OnButtonClicked(sender, e);
+
+            bool first = true;
+            Activated += (s, e) =>
+            {
+                if (first)
+                {
+                    first = false;
+                    Init_UI();
+                }
+            };
+            MessageBox.Show("更新完了");
+        }
+        private void SetPriority(object sender, RoutedEventArgs e)
+        {
+            module.SetPriority(sender, e);
+        }
+        private void Init_UI()
+        {
+            module.text_list = new List<TextBox>();
+            module.button_list = new List<Button>();
+            int changing = -1;
+            bool deadlook = true;
+            int i = 0; //←処理対象の行番号を入れる
+            for (i = 0; i < module.managedData.Count; ++i)
+            {
+
+                var listViewItem = listView.ItemContainerGenerator.ContainerFromIndex(i) as ListViewItem;
+                if (listViewItem == null)
+                {
+                    var items = listView.ItemsSource as ObservableCollection<wdata>;
+                    listView.ScrollIntoView(items[i]);
+                    listViewItem = listView.ItemContainerGenerator.ContainerFromIndex(i) as ListViewItem;
+
+                    RoutedEventHandler handler = null;
+                    handler = (object hs, RoutedEventArgs he) =>
+                    {
+                        //<< listViewItemに対する処理を実行 >>
+                        listViewItem.Loaded -= handler;
+                    };
+                    //listViewItem.Loaded += handler;
+                }
+                else
+                {
+                    //<< listViewItemに対する処理を実行 >>
+                    int columnNumber = 1; //←対象の列番号を入れる
+                    var rowPresenter = FindVisualChild<GridViewRowPresenter>(listViewItem);
+                    var cellPresenter = VisualTreeHelper.GetChild(rowPresenter, columnNumber) as ContentPresenter;
+                    var template = rowPresenter.Columns[columnNumber].CellTemplate;
+                    //var name = template.FindName("name", cellPresenter) as TextBlock; //コントロールの型;
+                    //<< コントロールに対する処理を実行 >> //【name】は上手くいかない
+                    var priority = template.FindName("priority", cellPresenter) as TextBox;
+                    int k = 0;
+                    while (priority == null && k < 100)
+                    {
+                        priority = template.FindName("priority" + k, cellPresenter) as TextBox;
+                        ++k;
+                    }
+                    if (priority != null)
+                    {
+                        System.Diagnostics.Trace.WriteLine(i + "番目 : " + module.managedData[i].productName);
+                        System.Diagnostics.Trace.WriteLine("priority : " + priority.Text + "!!!!!!!!!!!!!!!!!!!!");
+                        priority.Name = "priority" + i;
+                        module.text_list.Add(priority);
+                        if (priority.IsEnabled)
+                        {
+                            deadlook = false;
+                            changing = i;
+                            //System.Diagnostics.Trace.WriteLine("TextBox["+i+"] = True");
+                        }
+                    }
+                    else
+                        MessageBox.Show("Not Found : priority" + i);
+
+                    var button = template.FindName("button", cellPresenter) as Button;
+                    if (button == null)
+                        button = template.FindName("button" + (k - 1), cellPresenter) as Button;
+                    if (button != null)
+                    {
+                        System.Diagnostics.Trace.WriteLine("button : OK");
+                        button.Name = "button" + i;
+                        module.button_list.Add(button);
+                        if (button.IsEnabled)
+                        {
+                            deadlook = false;
+                            //System.Diagnostics.Trace.WriteLine("Button[" + i + "] = True");
+                        }
+                    }
+                    else
+                        MessageBox.Show("Not Found : button" + i);
+
+
+
+                    columnNumber = 2;
+                    cellPresenter = VisualTreeHelper.GetChild(rowPresenter, columnNumber) as ContentPresenter;
+                    template = rowPresenter.Columns[columnNumber].CellTemplate;
+                    var volume = template.FindName("volume", cellPresenter) as Slider;
+                    if (volume == null)
+                        volume = template.FindName("volume" + (k - 1), cellPresenter) as Slider;
+                    if (volume != null)
+                    {
+                        System.Diagnostics.Trace.WriteLine("volume : " + volume.Value + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                        volume.Name = "volume" + i;
+                    }
+                    else
+                        MessageBox.Show("Not Found : volume" + i);
+                }
+            }
+            if (deadlook)
+                for (i = 0; i < module.button_list.Count; ++i)
+                    module.button_list[i].IsEnabled = true;
+            if (changing == -1)
+                return;
+            for (i = 0; i < module.button_list.Count; ++i)
+                if (i != changing)
+                    module.button_list[i].IsEnabled = false;
+            //else
+            //    module.text_list[i].IsEnabled = true;
+        }
+        private T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                    return (T)child;
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
         }
     } // end of MainWindow class
 
