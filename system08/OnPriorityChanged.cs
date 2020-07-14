82ca70648399e607495d2b9d4c2246c918ba6bb0
toitenu <toitenu.tenuitto@gmail.com>
2020-07-14 15:35:32 +0900

    GetWindow.cs OnButtonClicked.cs OnPriorityChanged.cs の更新

--- system08/OnPriorityChanged.cs
+++ system08/OnPriorityChanged.cs
@@ -24,12 +24,12 @@ using system08;
 
 namespace system08
 {
-	partial class UIModule
-	{
+    partial class UIModule
+    {
         //優先度変更のButtonのList  Run()で読み込む予定
-        private List<Button> button_list = new List<Button>();
+        public List<Button> button_list = new List<Button>();
         //優先度のTextBoxのList  Run()で読み込む予定
-        private List<TextBox> text_list = new List<TextBox>();
+        public List<TextBox> text_list = new List<TextBox>();
 
         private int button_num = 0; //ボタン識別変数
         private bool enter = false; //Enter識別変数
@@ -42,7 +42,8 @@ namespace system08
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void SetPriority(object sender, RoutedEventArgs e)
+
+        public void SetPriority(object sender, RoutedEventArgs e)
         {
             var button = sender as Button;
             if (button == null)
@@ -50,17 +51,20 @@ namespace system08
             string str = button.Name.Remove(0, 6); //str.Replace("button", "");
             if (!int.TryParse(str, out button_num))
             {
-                MessageBox.Show(str);
+                MessageBox.Show("Not Integer : " + str);
                 return;
             }
-            Slider sl = new Slider();
-
             if (!priorityModule.CheckWindow(managedData[button_num].hwnd))
+            {
+                MessageBox.Show("Removed : " + str);
                 return;
+            }
+
             for (int i = 0; i < button_list.Count; ++i)
                 button_list[i].IsEnabled = false;
-            text_list[button_num].IsEnabled = true;
+            text_list[button_num].IsEnabled = true;///////////////////////////////
         }
+        //*/
 
 
         /// <summary>
@@ -72,9 +76,28 @@ namespace system08
         /// <param name="e"></param>
         public void PreviewPriorityText(object sender, TextCompositionEventArgs e)
         {
+            ///////////////////////////////////////////////
+            var text = sender as TextBox;
+            if (text == null)
+                return;
+            string str = text.Name.Remove(0, 8); //str.Replace("priority", "");
+            if (!int.TryParse(str, out button_num))
+            {
+                MessageBox.Show("Not Integer : " + str);
+                return;
+            }
+
+            if (!priorityModule.CheckWindow(managedData[button_num].hwnd))
+            {
+                MessageBox.Show("Removed : " + str);
+                return;
+            }
+            ///////////////////////////////////////////////////////
+
             bool flag;
             //入力文字列が２文字以下であるかを判定
-            switch (text_list[button_num].Text.Length)
+            //switch (text_list[button_num].Text.Length)
+            switch (text.Text.Length)
             {
                 case 0:
                     flag = !new Regex("[0-9]").IsMatch(e.Text);
@@ -114,19 +137,46 @@ namespace system08
         /// <param name="e"></param>
         public void OnPriorityChanged(object sender, KeyEventArgs e)
         {
+            ///////////////////////////////////////////////
+            var text = sender as TextBox;
+            if (text == null)
+            {
+                MessageBox.Show("Cannot Change");
+                return;
+            }
+            string str = text.Name.Remove(0, 8); //str.Replace("priority", "");
+            if (!int.TryParse(str, out button_num))
+            {
+                MessageBox.Show("Not Integer : " + str);
+                return;
+            }
+
+            if (!priorityModule.CheckWindow(managedData[button_num].hwnd))
+            {
+                MessageBox.Show("Removed : " + str);
+                return;
+            }
+            ///////////////////////////////////////////////
+
             int num = button_num;
-            if (e.Key == Key.Return && text_list[num].Text.Length >= 1)
+            //if (e.Key == Key.Return && text_list[num].Text.Length >= 1)
+            if (e.Key == Key.Return && text.Text.Length >= 1)
             {
                 enter = true;
-                if (text_list[num].Text[0] == '0' && text_list[num].Text.Length == 2)
-                    text_list[num].Text = text_list[num].Text[1] + "";
+                //if (text_list[num].Text[0] == '0' && text_list[num].Text.Length == 2)
+                if (text.Text[0] == '0' && text.Text.Length == 2)
+                    //text_list[num].Text = text_list[num].Text[1] + "";
+                    text.Text = text.Text[1] + "";
                 text_list[num].IsEnabled = false;
 
                 //優先度を保持
-                managedData[num].priority = int.Parse(text_list[num].Text);
+                //managedData[num].priority = int.Parse(text_list[num].Text);
+                managedData[num].priority = int.Parse(text.Text);
 
                 //ウィンドウ切り替え関数()  ただし優先度選択画面が最前面
-                priorityModule.assignPriority(managedData[num].priority, managedData);
+                //priorityModule.assignPriority(managedData[num].priority, managedData);
+                System.Diagnostics.Trace.WriteLine("push");
+                priorityModule.assignPriority(managedData[num].hwnd, managedData[num].priority, managedData);
 
                 //優先度変更ボタンを有効にする
                 for (int i = 0; i < button_list.Count; ++i)
@@ -135,7 +185,7 @@ namespace system08
         }
 
 
- 
+
         /// <summary>
         /// 貼り付けを許可しない
         /// </summary>
@@ -147,6 +197,10 @@ namespace system08
             {
                 e.Handled = true;
             }
+        }
+        public void SetThisWindowHandle(IntPtr hWnd)
+        {
+            priorityModule.this_window_hwnd = hWnd;
         }
     }
 }
